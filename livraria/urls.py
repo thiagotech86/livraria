1c3237ff928d9bba6b22ac3fdd1c166ec652cdd1
thiago_martins <thiago86.tech@gmail.com>
2025-03-15 12:34:23 -0300

    Construção do NavBar

--- livraria/urls.py
+++ livraria/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
 from django.http import HttpResponse
-from livraria.views import home, sobre, logout_user
+from livraria.views import home, register_user, logout_user
 
 
 urlpatterns=[
     path('', home, name='home'), # Utiliza-se name para chamar a rota através do link
-    path('sobre/', sobre),
+    path('register/', register_user, name='register'),
     path('logout/',logout_user, name='logout')
 
 ]
--- livraria/views.py
+++ livraria/views.py
@@ -1,15 +1,36 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-
+from django.shortcuts import render, redirect # type: ignore
+from django.http import HttpResponse # type: ignore
+from django.contrib.auth import authenticate, login, logout # type: ignore
+from django.contrib import messages # type: ignore # mensagens de erro 
 # Retornando uma página html
 def home(request):
-    return render(request,'home.html') 
+    if request.method=="POST": # Se o método request for post, valide login e senha, se não retorne a página home.
+        username=request.POST['usuario']
+        password=request.POST['senha']
+        #Autenticação
+        user=authenticate(
+            request,
+            username=username,
+            password=password
+        )
+        if user is not None: # condição após a validação
+            login(request,user)
+            messages.success(request,"Login realizado com sucesso!") # mensagem de confirmação de login
+            return redirect('home')
+        else:
+            messages.error(request,"Erro na autenticação. Tente novamente!") # mensagem de erro de login
+            return redirect('home')
+
+    else:
+        return render(request,'home.html') 
 
 
 def logout_user(request):
-    pass
+    logout(request)
+    messages.success(request, "Logout com sucesso!")
+    return redirect('home')
 
 
 
-def sobre(request):
-    return HttpResponse('Teste Sobre')
+def register_user(request):
+    return render(request,"register.html")
